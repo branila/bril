6b29f77c4153f9b49ae4a678579f0bd2ab89c9c2
branilaa <stefan.branila55@gmail.com>
2024-09-16 19:54:29 +0200

    included database initialization

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/branila/bril/types"
+	"github.com/branila/bril/db"
 )
 
 func main() {
-	t := types.Task{
-		Id:       0,
-		Name:     "Create an advanced terminal-centric task manager",
-		Done:     false,
-		Priority: 100,
-		Tag:      types.Tag{},
-		Notes: []types.Note{
-			{
-				Text:     "This is a note",
-				Creation: time.Now(),
-			},
-		},
-		Creation:   time.Now(),
-		Completion: time.Time{},
-		Expiration: time.Time{},
-		Elapsed:    0,
-	}
-
-	fmt.Println(t)
+	db.Init()
 }
