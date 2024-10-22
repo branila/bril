de65b14b169eff1e90bae38d963ffe3fa76c1358
branila <stefan.branila55@gmail.com>
2024-10-22 16:33:58 +0200

    implemented task completion command

--- handlers/done/done.go
+++ handlers/done/done.go
@@ -1,5 +1,41 @@
 package handlers
 
-func Done() {
+import (
+	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/branila/bril/db"
+)
+
+func Done() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: bril done <task id>")
+		return
+	}
+
+	id, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("'%s' is not a valid task id\n", os.Args[2])
+		return
+	}
+
+	if id < 0 || id >= len(db.GetTasks()) {
+		fmt.Printf("Task %d not found", id)
+		return
+	}
+
+	tasks := db.GetTasks()
+
+	if tasks[id].Done {
+		fmt.Printf("Task %d already done\n", id)
+		return
+	}
+
+	tasks[id].Done = true
+	tasks[id].Deleted = false
+
+	db.SyncDb()
+
+	fmt.Printf("Task %d marked as done\n", id)
 }
